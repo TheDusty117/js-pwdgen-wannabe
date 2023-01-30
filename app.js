205f2d927c64eec469270bef431fb0a86c25a8f9
TheDusty117 <gabrieledipassio@gmail.com>
2023-01-30 15:15:34 +0100

    FEAT asking stuff done, need to overwrite name

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+
+
 // chiedi il nome utente
 
 let name = prompt ("quale e' il tuo nome?")
 
+document.getElementById("main_title").value = name
 // chiedi il cognome utente
 
 let lastName = prompt ("qual e' il tuo cognome?")
 
 // chiedi il colore preferito
+
+let favouriteColor = prompt ("qual e' il tuo colore preferito?")
+
 // chiedi un numero
+
+let askNumber = prompt ("qual e' il tuo numero?")
+
+
 // scrivi sulla pagina il risultato concatenato degli elementi
