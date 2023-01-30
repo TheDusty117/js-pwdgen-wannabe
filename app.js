58083e48e32204bc876b8eba33764ff91977b0b2
TheDusty117 <gabrieledipassio@gmail.com>
2023-01-30 15:20:08 +0100

    FEAT pass created from lets??

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
 
 let name = prompt ("quale e' il tuo nome?")
 
-document.getElementById("main_title").innerHTML = name
 // chiedi il cognome utente
 
 let lastName = prompt ("qual e' il tuo cognome?")
 
+
 // chiedi il colore preferito
 
-let favouriteColor = prompt ("qual e' il tuo colore preferito?")
+let favColor = prompt ("qual e' il tuo colore preferito?")
 
 // chiedi un numero
 
@@ -19,3 +19,5 @@ let askNumber = prompt ("qual e' il tuo numero?")
 
 
 // scrivi sulla pagina il risultato concatenato degli elementi
+document.getElementById("main_title").innerHTML = 
+name + lastName + favColor + askNumber
