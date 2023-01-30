b41ad72573848de80cc684ebe6580663d61923a4
TheDusty117 <gabrieledipassio@gmail.com>
2023-01-30 15:07:11 +0100

    FEAT name/lastname prompt active

---
+++ app.js
@@ -0,0 +1,11 @@
+// chiedi il nome utente
+
+let name = prompt ("quale e' il tuo nome?")
+
+// chiedi il cognome utente
+
+let lastName = prompt ("qual e' il tuo cognome?")
+
+// chiedi il colore preferito
+// chiedi un numero
+// scrivi sulla pagina il risultato concatenato degli elementi
